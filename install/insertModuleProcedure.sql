a33a2a95474e333e85fd9add411f2288f25e1630
Hitesh Gupta <hiteshgupta440@gmail.com>
2023-01-20 12:37:57 +0530

    Update store procedure SQL file

--- install/insertModuleProcedure.sql
+++ install/insertModuleProcedure.sql
@@ -1,15 +1,11 @@
 
-DROP PROCEDURE IF EXISTS insert_module;
-DELIMITER $$
 CREATE PROCEDURE insert_module(
   IN my_data JSON
 )
 BEGIN
-  -- Declare iterator variable to use it later on in the loop
   DECLARE i INT DEFAULT 0;
   DECLARE j INT DEFAULT 0;
 
-  -- Retrieve values from JSON
   SET @subject = JSON_UNQUOTE(JSON_EXTRACT(my_data, '$.subject'));
   SET @description = JSON_UNQUOTE(JSON_EXTRACT(my_data, '$.description'));
   SET @due_date = JSON_UNQUOTE(JSON_EXTRACT(my_data, '$.due_date'));
@@ -18,7 +14,7 @@ BEGIN
   SET @priority = JSON_UNQUOTE(JSON_EXTRACT(my_data, '$.priority'));
   SET @created_by = JSON_UNQUOTE(JSON_EXTRACT(my_data, '$.created_by'));
   SET @notes = JSON_EXTRACT(my_data, '$.note');
-  -- Insert module
+
   INSERT INTO `modules`(`subject`, `description`, `startDate`, `dueDate`, `status`, `priority`, `createdBy`) VALUES (
     @subject,
     @description,
@@ -28,19 +24,19 @@ BEGIN
     @priority,
     @created_by
     );
-  -- Retrieve inserted id to reuse it in module_note
+  
   SET @last_module = LAST_INSERT_ID();
 
-  -- Get notes length for the loop
+
   SET @notes_length = JSON_LENGTH(@notes);
-  -- Execute loop over notes length
+ 
   WHILE i < @notes_length DO
-    -- Retrieve current note from notes array
+    
     SET @note_subject = JSON_UNQUOTE(JSON_EXTRACT(my_data, CONCAT('$.note[',i,'].subject')));
     SET @note = JSON_UNQUOTE(JSON_EXTRACT(my_data, CONCAT('$.note[',i,'].note')));
     SET @attachment = JSON_UNQUOTE(JSON_EXTRACT(my_data, CONCAT('$.note[',i,'].attachment')));
    
-    -- Insert Note
+    
    
     INSERT INTO `notes`(`subject`, `note`, `moduleId`, `createdBy`) VALUES (
       @note_subject,
@@ -61,11 +57,10 @@ BEGIN
           @attachment_file,
           @last_note
         );
-     -- -- Add step to iterator
+     
       SELECT j + 1 INTO j;
     END WHILE;   
-    -- -- Add step to iterator
+    
     SELECT i + 1 INTO i;
   END WHILE;
-END $$
-DELIMITER ;
+END ;
